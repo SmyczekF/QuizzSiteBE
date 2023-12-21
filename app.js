01a211e3a29faa2260c9e9f7ecf2cb2a0b835250
Filip Smyczek <smyczekf@interia.pl>
2023-12-21 15:10:08 +0100

    Added node app

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use('/', express.static('build'))
+app.get('/backend', (req, res) => res.send('Hello World!'))
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
